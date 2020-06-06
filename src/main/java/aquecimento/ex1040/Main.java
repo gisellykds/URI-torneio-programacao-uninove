3c79e044bf98034a8a6c153e02da27b6261e9d15
Giselly Silva <gisellykathellynsilva@gmail.com>
2020-06-06 15:51:37 -0300

    Testando novo diret

--- src/main/java/aquecimento/ex1040/Main.java
+++ src/main/java/aquecimento/ex1040/Main.java
@@ -2,6 +2,11 @@ package main.java.aquecimento.ex1040;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Main main = new Main();
+        main.media3();
+    }
+    
+    public void media3(){
+       // testando novo diretorio
     }
 }
